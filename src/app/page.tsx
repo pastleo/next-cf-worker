dba9389e00af10ecb657a373264743e26e6c1386
PastLeo <chgu82837@gmail.com>
2025-08-20 13:07:16 +0800

    deploy to cf worker with gh-action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">
+            NEXT_PUBLIC_ENV: {process.env.NEXT_PUBLIC_ENV}
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
